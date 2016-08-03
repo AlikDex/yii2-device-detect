18e0f065366d6d7ba9be6e31ddf39f56917af312
AlikDex <alikdexor@gmail.com>
2016-08-03 14:36:50 +0700

    Update DeviceDetect.php

--- DeviceDetect.php
+++ DeviceDetect.php
@@ -12,9 +12,6 @@ class DeviceDetect extends \yii\base\Component {
 	// Automatically set view parameters based on device type
 	public $setParams = false;
 
-	// Automatically set alias parameters based on device type
-	public $setAlias = false;
-
 	public function __call($name, $parameters) {
 		return call_user_func_array(
 			array($this->_mobileDetect, $name),
@@ -39,16 +36,6 @@ class DeviceDetect extends \yii\base\Component {
 			\Yii::$app->params['devicedetect']['isDesktop'] =
 				!\Yii::$app->params['devicedetect']['isMobile'] &&
 				!\Yii::$app->params['devicedetect']['isTablet'];
-		}
-
-		if ($this->setAlias) {
-			if ($this->_mobileDetect->isMobile()) {
-					\Yii::setAlias('@device', 'mobile');
-			} else if ($this->_mobileDetect->isTablet()) {
-					\Yii::setAlias('@device', 'tablet');
-			} else {
-					\Yii::setAlias('@device', 'desktop');
-			}
 		}
 	}
 
